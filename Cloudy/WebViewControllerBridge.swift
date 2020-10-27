b3645b914ce6c2267f407b44fa57b9c97999f882
Martin Mlostek <mlostek@gmail.com>
2020-10-27 23:00:47 +0100

    intelligent sending of "changes" of the controller

--- Cloudy/WebViewControllerBridge.swift
+++ Cloudy/WebViewControllerBridge.swift
@@ -8,22 +8,26 @@ import GameController
 /// Main module that connects the web views controller scripts to the native controller handling
 class WebViewControllerBridge: NSObject, WKScriptMessageHandlerWithReply {
 
-    /// Remember last pressed keys to avoid log spamming
-    private var lastPressedKeys: String? = nil
+    /// Remember last controller snapshot
+    var lastControllerSnapshot: GCExtendedGamepad? = nil
 
     /// Handle user content controller with proper native controller data reply
     func userContentController(_ userContentController: WKUserContentController,
                                didReceive message: WKScriptMessage,
                                replyHandler: @escaping (Any?, String?) -> Void) {
-        #if DEBUG
-            // debug log
-            if let pressedKeys = GCController.controllers().first?.extendedGamepad?.pressedKeys {
-                if lastPressedKeys != pressedKeys {
-                    print(pressedKeys)
-                }
-                lastPressedKeys = pressedKeys
-            }
-        #endif
-        replyHandler(GCController.controllers().first?.extendedGamepad?.toJson(), nil)
+        // early exit
+        guard let currentControllerState = GCController.controllers().first?.extendedGamepad else {
+            replyHandler(nil, nil)
+            return
+        }
+        // nothing changed, skip
+        if let lastControllerState = lastControllerSnapshot,
+              lastControllerState =~ currentControllerState {
+            replyHandler(nil, nil)
+            return
+        }
+        // update and save
+        lastControllerSnapshot = currentControllerState.capture()
+        replyHandler(currentControllerState.toJson(), nil)
     }
 }
