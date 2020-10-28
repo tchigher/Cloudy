2ae4faab6f5cc20d3a25e850e2794e4ff183e94d
Martin Mlostek <mlostek@gmail.com>
2020-10-28 09:53:07 +0100

    extracted some sensitive functionality to precompiled library

--- Cloudy/Extension/GameController.swift
+++ Cloudy/Extension/GameController.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 import GameController
+import CloudyLibrary
 
 /// Infix operator declaration
 infix operator =~: ComparisonPrecedence
@@ -14,11 +15,6 @@ extension GCControllerButtonInput {
         lhs.isPressed == rhs.isPressed &&
         lhs.isTouched == rhs.isTouched &&
         lhs.value =~ rhs.value
-    }
-
-    /// Convenience creator
-    var controller: Controller.Button {
-        Controller.Button(pressed: isPressed, touched: isTouched, value: value)
     }
 }
 
@@ -33,10 +29,6 @@ extension Float {
 
 /// Convenience extension
 extension GCExtendedGamepad {
-
-    /// Hacked pulsing
-    /// TODO find a proper solution
-    private static var pulse: Bool = false
 
     /// Check all values for similarity
     static func =~(lhs: GCExtendedGamepad, rhs: GCExtendedGamepad) -> Bool {
@@ -72,48 +64,5 @@ extension GCExtendedGamepad {
             return false
         }
         return true
-    }
-
-    /// Constant static id
-    static var id: String {
-        "Xbox Wireless Controller (STANDARD GAMEPAD Vendor: 045e Product: 02fd)"
-    }
-
-    /// Convert to json
-    func toJson() -> String? {
-        guard let buttonOptions = buttonOptions,
-              let buttonHome = buttonHome,
-              let leftThumbstickButton = leftThumbstickButton,
-              let rightThumbstickButton = rightThumbstickButton else {
-            return nil
-        }
-        GCExtendedGamepad.pulse = !GCExtendedGamepad.pulse
-        return Controller(
-                axes: [
-                    leftThumbstick.xAxis.value,
-                    -1.0 * leftThumbstick.yAxis.value,
-                    rightThumbstick.xAxis.value,
-                    -1.0 * rightThumbstick.yAxis.value
-                ],
-                buttons: [
-                    /*  0 */ buttonA.controller,
-                    /*  1 */ buttonB.controller,
-                    /*  2 */ buttonX.controller,
-                    /*  3 */ buttonY.controller,
-                    /*  4 */ leftShoulder.controller,
-                    /*  5 */ rightShoulder.controller,
-                    /*  6 */ Controller.Button(pressed: leftTrigger.isPressed, touched: leftTrigger.isTouched, value: max(leftTrigger.value - 0.002, 0) + (GCExtendedGamepad.pulse ? 0.002 : 0)),
-                    /*  7 */ rightTrigger.controller,
-                    /*  8 */ buttonOptions.controller,
-                    /*  9 */ buttonMenu.controller,
-                    /* 10 */ leftThumbstickButton.controller,
-                    /* 11 */ rightThumbstickButton.controller,
-                    /* 12 */ dpad.up.controller,
-                    /* 13 */ dpad.down.controller,
-                    /* 14 */ dpad.left.controller,
-                    /* 15 */ dpad.right.controller,
-                    /* 16 */ buttonHome.controller,
-                ])
-                .jsonString
     }
 }
